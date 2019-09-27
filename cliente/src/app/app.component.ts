d850c2e04ac6c391d57c3eca60dd9ef8b45a10f7
Programando Ideas <programando.ideas123@gmail.com>
2019-09-27 16:26:56 -0500

    Correccion de errores por comando "ng build --prod" Actualización de version de Angular CLI

--- cliente/src/app/app.component.ts
+++ cliente/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { usuario } from './models/usuario';
 })
 export class AppComponent {
   title = 'cliente';
-
+  isLoginProcessing = false;
   Authenticated = false;
   loginForm: FormGroup;
   public _usuario: usuario = { usuario: '', pwd: '', id: 0, nombres: '', apellidos: '', email: '', telefono: '' };
@@ -25,7 +25,9 @@ export class AppComponent {
   }
   
   public onSubmit(form: any) {
+    this.isLoginProcessing = true;
     this.Authenticated = true;
+    this.isLoginProcessing = false;
     this._router.navigate(['/home']);
   }
 }
--- cliente/src/environments/environment.prod.ts
+++ cliente/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  urlapi: "https://localhost:5001/api/"
 };
